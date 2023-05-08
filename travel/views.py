bac0f9471eb08fb49a251e6b2ffa7aa0eb069a27
Sooshieroll <lucyy.wang95@gmail.com>
2023-05-07 17:36:14 -0700

    profile wip

--- travel/views.py
+++ travel/views.py
@@ -1,5 +1,8 @@
-from django.shortcuts import HttpResponse
+from django.shortcuts import HttpResponse, render
 
 # Create your views here.
 def index(request):
     return HttpResponse("Hello world. You're at the travel index.")
+
+def profile_view(request):
+    return render(request, 'users/profile.html')
